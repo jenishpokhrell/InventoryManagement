5b078f455ab36e11ffe050bbec3a67b3a8489c04
Jenish Pokhrel <susantp056@gmail.com>
2024-09-25 20:46:53 +0545

    Add Product and View

--- InventoryManagementSystem/Controllers/ProductsController.cs
+++ InventoryManagementSystem/Controllers/ProductsController.cs
@@ -1,10 +1,18 @@
-﻿using InventoryManagementSystem.Models;
+﻿using InventoryManagementSystem.Data;
+using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagementSystem.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ApplicationDBContext dbContext;
+        public ProductsController(ApplicationDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
@@ -12,9 +20,26 @@ namespace InventoryManagementSystem.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(AddProductViewModel viewModel)
+        public async Task<IActionResult> Add(AddProductViewModel viewModel)
         {
+            var product = new Product
+            {
+                ProductName = viewModel.ProductName,
+                Description = viewModel.Description,
+                Location = viewModel.Location,
+                Quantity = viewModel.Quantity,
+                Price = viewModel.Price,
+            };
+            await dbContext.Products.AddAsync(product);
+            await dbContext.SaveChangesAsync();
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var students = await dbContext.Products.ToListAsync();
+            return View(students);
         }
     }
 }
